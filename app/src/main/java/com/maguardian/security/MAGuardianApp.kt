9d1bfedb3255a25677f8c34cbd3700b406261297
Anderraupp <121187360+Anderraupp@users.noreply.github.com>
2026-05-13 01:32:55 -0300

    Inicializa TikTok Business SDK

--- app/src/main/java/com/maguardian/security/MAGuardianApp.kt
+++ app/src/main/java/com/maguardian/security/MAGuardianApp.kt
@@ -2,11 +2,18 @@ package com.maguardian.security
 
 import android.app.Application
 import com.facebook.appevents.AppEventsLogger
+import com.tiktok.sdk.business.TikTokBusinessSdk
 
 class MAGuardianApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         AppEventsLogger.activateApp(this)
+
+        val tiktokConfig = TikTokBusinessSdk.TTConfig(applicationContext)
+            .setAppId("7639159884537266194")
+            .setAccessToken("TTnIwdaW9PIUIhjzcdfFYRwH7yKEDQYL")
+        TikTokBusinessSdk.initializeSdk(tiktokConfig)
     }
 }
